3e1412582b26200625d065e0d1e8e81327cad5c3
artikoglueksik <ahmetbgc@proton.me>
2025-12-28 23:52:00 +0300

    Blog update 12/28/2025 11:52pm: 2 file(s) changed.

--- src/config.ts
+++ src/config.ts
@@ -314,7 +314,7 @@ export const siteConfig: SiteConfig = {
       { title: "Posts", url: "/posts/" },
       { title: "Projects", url: "/projects/" },
       { title: "Docs", url: "/docs/" },
-      { title: "Curriculum Vitae", url: "/curriculum vitae/" },
+      { title: "About", url: "/about/" },
       { title: "GitHub", url: "https://www.reddit.com/r/withastro/comments/1pf2e4f/introducing_vault_cms_use_obsidian_to_write_and/" }
     ],
     // [CONFIG:NAVIGATION_SOCIAL]
